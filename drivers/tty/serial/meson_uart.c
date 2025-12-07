7fa86864638bbe565b81f36dc1b664fe75c6e680
hj-wrt <mincoek76@gmail.com>
2025-12-07 19:09:55 +0700

    Update meson_uart.c

--- drivers/tty/serial/meson_uart.c
+++ drivers/tty/serial/meson_uart.c
@@ -824,6 +824,7 @@ static const struct of_device_id meson_uart_dt_match[] = {
 	{ .compatible = "amlogic,meson8-uart" },
 	{ .compatible = "amlogic,meson8b-uart" },
 	{ .compatible = "amlogic,meson-gx-uart" },
+        { .compatible = "amlogic,meson-gxl-uart" },
 	{
 		.compatible = "amlogic,meson-g12a-uart",
 		.data = (void *)&meson_g12a_uart_data,
